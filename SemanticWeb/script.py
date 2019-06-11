670c4b1c0b3163cea98156a484ea52d1c36f45c6
enrycoop <enricocoppolecchia@outlook.com>
2019-06-11 21:08:31 +0200

    New target variable added. Test implemented.

--- SemanticWeb/script.py
+++ SemanticWeb/script.py
@@ -11,14 +11,17 @@ sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
 sparql.setQuery("""
    PREFIX dbpedia: <http://dbpedia.org/ontology/>
-   SELECT DISTINCT ?mag ?apoapsis ?escape ?rotPer
+   SELECT DISTINCT ?mag ?apoapsis ?escape ?rotPer ?orbitalPer ?target
    WHERE{
       ?CelestialBody a dbpedia:Planet.
       ?CelestialBody dbpedia:absoluteMagnitude ?mag.
       ?CelestialBody dbpedia:apoapsis ?apoapsis.
       ?CelestialBody dbpedia:escapeVelocity ?escape.
       ?CelestialBody dbpedia:rotationPeriod ?rotPer.
-      
+      ?CelestialBody dbpedia:orbitalPeriod ?orbitalPer.
+      OPTIONAL{
+       ?CelestialBody dbpedia:averageSpeed ?target.
+      }
    } 
 """)
 
@@ -26,33 +29,37 @@ sparql.setQuery("""
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
-#now I'll create the dataset
-#['mag']['value']
-#['apoapsis']['value']
-#['rotPer']['value']
-#['escape']['value']
+#[creation of the dataset]
 
 import numpy as np
 
 X = []
 y = []
+X_void =[]
+
 for result in results["results"]["bindings"]:
-   X.append([float(result['apoapsis']['value']),float(result['rotPer']['value']),float(result['escape']['value'])])
-   y.append(float(result['mag']['value']))
+   
+   if 'target' not in result:
+      X_void.append([float(result['mag']['value']),float(result['apoapsis']['value']),float(result['escape']['value']),float(result['rotPer']['value']),float(result['orbitalPer']['value'])])
+   else:
+      X.append([float(result['mag']['value']),float(result['apoapsis']['value']),float(result['escape']['value']),float(result['rotPer']['value']),float(result['orbitalPer']['value'])])
+      y.append(float(result['target']['value']))
+      
+
 X = np.array(X)
 y = np.array(y)
 print(f'total samples downloaded: {len(y)}')
-features = [('dbpedia:apoapsis','A'),('dbpedia:rotationPeriod','R'),('dbpedia:escapeVelocity','E')]
-target = 'dbpedia:absoluteMagnitude'
+features = [('dbpedia:absoluteMagnitude','M'),('dbpedia:apoapsis','A'),('dbpedia:rotationPeriod','R'),('dbpedia:escapeVelocity','E'),('dbpedia:orbitalPeriod','O')]
+target = 'dbpedia:averageSpeed'
 
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
-
+'''
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=33)
 
 print(f"x_train:{len(y_train)}\nx_test:{len(y_test)}")
 for i in range(len(features),len(features)*4):
-   regr = DecisionTreeRegressor(max_depth=i)
+   regr = DecisionTreeRegressor(max_depth=i,min_samples_split=0.1,criterion='friedman_mse')
    regr.fit(X_train, y_train)
 
    r_tests = regr.predict(X_test)
@@ -63,11 +70,11 @@ for i in range(len(features),len(features)*4):
    print(f"mse({i}): {tot}")
 y = np.array(y_test)
 print(f"max: {y.max()} - min: {y.min()} - mean: {y.mean()}")
-
-
 '''
+
+regr = DecisionTreeRegressor(criterion='friedman_mse',max_depth=len(features),min_samples_split=0.1)
+regr.fit(X, y)
 with open('rules_result.rules','w') as f:
    for rule in get_rules(regr,features,target):
       f.write(rule+'\n\n')
-'''
 
